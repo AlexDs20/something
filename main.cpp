535a9a57224b002e318dcda46778b63153d498f1
Alexandre De Spiegeleer <alex-ds@live.be>
2025-05-09 20:11:39 +0200

    memset framebuffer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <unistd.h>         // STDOUT_FILENO
 #include <sys/syscall.h>    // syscall()
 #include <stdlib.h>
+#include <string.h>
 
 #include "platform/window.h"
 #include "renderer/renderer.h"
@@ -19,7 +20,10 @@ int main() {
     const uint32 w = 1024;
     const uint32 h = 768;
 
+    const uint32 bg_color = 0x00FF0000;
+
     uint32* framebuffer = (uint32*)malloc(w*h*sizeof(uint32));
+    memset(framebuffer, bg_color, w*h*sizeof(uint32));
 
     uint32 running = 1;
     while (running) {
@@ -27,9 +31,11 @@ int main() {
         break;
     }
 
+    // platform_main();
 
     free_model(model);
     free(framebuffer);
 
-    // platform_main();
+    char* done_msg = "DONE!\n";
+    syscall(1, STDOUT_FILENO, done_msg, sizeof(done_msg)-1);
 }
