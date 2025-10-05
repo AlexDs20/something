228b97b32e044ea47aafa312ea8781c67c24feea
Alexandre De Spiegeleer <alex-ds@live.be>
2025-10-05 16:12:12 +0200

    WIP

--- main.cpp
+++ main.cpp
@@ -26,37 +26,18 @@ int main() {
     Arena* scene_arena = arena_alloc_create(1*GiB);
     Arena* frame_arena = arena_alloc_create(1*GiB);
 
-    // string8 file_path = string_from_cstr(scene_arena, "assets/backpack/backpack.obj");
-    // Model* model = {0};
-    // if (string_get_file_extension(file_path) == ".obj") {
-    //     model = read_obj_model_file(scene_arena, file_path);
-    // }
+    string8 file_path = string_from_cstr(scene_arena, "assets/backpack/backpack.obj");
+    Model* model = {0};
+    if (string_get_file_extension(file_path) == ".obj") {
+        model = read_obj_model_file(scene_arena, file_path);
+    }
 
-    string8 file_path = string_from_cstr(scene_arena, "assets/backpack/diffuse.jpg");
-
-    Image image = read_image_file(global_arena, file_path);
-
-#if 1
-    // u32* img_b = model->material->map_Kd.data;
-    // u32  img_w = model->material->map_Kd.width;
-    // u32  img_h = model->material->map_Kd.height;
-
-    u32* img_b = image.buffer;
-    u32  img_w = image.width;
-    u32  img_h = image.height;
-    const u32 w = img_w;
-    const u32 h = img_h>512 ? 512 : img_h;
-    // const u32 w = 4096;
-    // const u32 h = 1024;
-
+    const u32 w = 1024;
+    const u32 h = 768;
     const u32 bg_color = 0x777777;
-
     Win win = platform_init_win(w, h, msg);
-    memcpy(win.buffer, img_b, w*h*4);
-    // memcpy(win.buffer, model->material->map_Kd.data, w*h*4);
-    // win.buffer = model->material->map_Kd.data;
 
-    f32Bits zdefault = {.u = 0xFF7FFFFF};       // -Inf for IEEE 754 standard
+    const f32Bits zdefault = {.u = 0xFF7FFFFF};       // -Inf for IEEE 754 standard
 
     u32 running = 1;
     while (running) {
@@ -64,21 +45,20 @@ int main() {
         running = platform_handle_events(&win);
 
         // And this is uglier...
-        // f32* zbuffer = (f32*)arena_alloc_push(frame_arena, win.h*win.w*sizeof(f32));
+        f32* zbuffer = (f32*)arena_alloc_push(frame_arena, win.h*win.w*sizeof(f32));
 
-        // for (int i=0; i<win.h*win.w; i++) {
-        //     win.buffer[i] = bg_color;
-        //     zbuffer[i] = zdefault.f;
-        // }
+        for (int i=0; i<win.h*win.w; i++) {
+            win.buffer[i] = bg_color;
+            zbuffer[i] = zdefault.f;
+        }
 
         // draw_model_wireframe(model, win.w, win.h, win.buffer);
-        // draw_model(model, win.w, win.h, win.buffer, zbuffer);
+        draw_model(model, win.w, win.h, win.buffer, zbuffer);
 
         XPutImage(win.display, win.window, win.gc, win.xim, 0, 0, 0, 0, win.w, win.h);
         // usleep(16);
         // break;
     }
-#endif
 
     char done_msg[] = "Done doing something!\n";
     syscall(1, STDOUT_FILENO, done_msg, sizeof(done_msg)-1);
